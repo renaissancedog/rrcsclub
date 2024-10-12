f472a11881a5f31ff812f05fdfcc2fdaa3308438
William Liu <orangetnt827@gmail.com>
2024-10-12 10:17:47 -0500

    change main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,153 @@
-<script lang="ts">
-  import { base } from '$app/paths';
+<script>
+  // import Navbar from "../components/navbar";
+  const apply_link = "https://forms.gle/i3ncV7JumfaodDXK8";
+  const contact_link = "https://forms.gle/QkcoFtm9A6woxrs28";
+  const constitution_link = "https://github.com/RR-CS-Club/Club-Constitution";
 </script>
-<h1 class="text-center">Welcome to SvelteKit</h1>
-<p class="bg-blue-600 p-2">Visit <a class="underline text-blue-100" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<a class="underline text-blue-500 pr-4" href="{base}/main">main</a>
+
+<html lang="en">
+<!-- <script defer data-domain="rrcsclub.vercel.app" src="https://plausible.io/js/plausible.js"></script> -->
+<title>Ronald Reagan High School Computer Science Club</title>
+<meta name='description' content='The home page for the Ronald Reagan High School Computer Science club. We are a club for creative coding and Computer Science exploration. Join us to gain pratical experience in Computer Science and serve the community while having tons of fun!'/>
+<!-- <link rel='icon' href='/favicon.ico' /> -->
+<link rel='preconnect' href='https://fonts.gstatic.com' />
+<div class="bg-white flex flex-col justify-center items-center w-screen">
+  <!-- <Navbar /> -->
+  <section class="h-[30rem] bg-cover bg-[url('/TopBg.svg')] flex flex-col items-center justify-center w-screen bg-gray-200 bg-no-repeat z-0">
+    <h1 class="text-7xl h-32 pb-16 relative m-0 text-blue-800 uppercase text-center flex flex-col top-0 justify-center"> Reagan CS Club </h1>
+    <div class="text-4xl text-center leading-normal text-gray-800">
+      The club for <span class="rounded-lg text-pink-700 bg-pink-200 font-bold">creative coding,</span>
+      <span class="rounded-lg text-orange-500 bg-yellow-200 font-bold">collaboration
+      </span>
+      and
+      <span class="rounded-lg text-blue-600 bg-green-200 font-bold">
+        project-led learning
+      </span>
+      .
+    </div>
+    <div class="text-xl flex flex-row items-center justify-center">
+      <div
+      class="flex justify-center items-center bg-[url('/ButtonBg.svg')] h-28 w-60 my-8 mx-12 rounded-lg cursor-pointer uppercase text-white text-center bg-gray-800 bg-contain bg-no-repeat">
+      <h1 class="text-3xl text-center ApplyText">Apply</h1>
+    </div>
+      <div
+      class="flex justify-center items-center bg-[url('/ButtonBg.svg')] h-28 w-60 my-8 mx-12 rounded-lg cursor-pointer uppercase text-white text-center bg-gray-800 bg-contain bg-no-repeat">
+      <h1 class="text-3xl text-center ContactText">Contact</h1>
+    </div>
+</div>
+</section>
+<main class="bg-teal-900 w-screen flex flex-col justify-center items-center">
+  <section class="drop-shadow-2xl flex flex-col items-center justify-center">
+    <h1 class="rounded-2xl m-12 p-6 text-4xl text-teal-300 uppercase font-black bg-blue-700">Who We Are</h1>
+    <p class="m-12 w-fit text-2xl text-white font-light">
+      We are a passionate community of coders, designers and explorers
+      who like to learn by doing. The core philosophy of this club is
+      building fun, real-world projects to learn coding. Coding is a
+      superpower and it shows that you do not need much to change the
+      world — just a computer or even a phone and a creative mind.
+      <br />
+      <br /> Coding has become an essential skill for thousands of
+      professions and technological literacy is absolutely necessary in
+      today’s world where technology is a key aspect of our everyday
+      lives. We aim to bring this necessary technological literacy to
+      everyone so that no one is left behind in this revolution.
+    </p>
+  </section>
+  <h1 class="rounded-2xl m-12 p-6 text-4xl text-teal-300 uppercase font-black bg-blue-700">What we do</h1>
+  
+  <!-- <section class={styles.section}>
+    <h1 class={styles.sectionSubtitle}>Club Workshop Meetings</h1>
+    <section id={styles.cardSection}>
+      <div class={styles.card}>
+        <h2 class='Initiation'>Initiation</h2>
+        <p class='InitiationText'>
+          The Club leader guides everyone through a new topic. Everyone
+          writes some code and understands the basics of the topic.
+        </p>
+      </div>
+      <div class={styles.card}>
+        <h2 class='Creation'>Creation</h2>
+        <p class='CreationText'>
+          Everyone starts <br />
+          creating, in teams or on <br />
+          their own on a project
+          <br /> of their choice. The club <br />
+          officers guide everyone <br />
+          through projects.
+        </p>
+      </div>
+      <div class={styles.card}>
+        <h2>Demos</h2>
+        <p class='DemoText'>
+          Anyone who wishes to
+          <br /> can present what they
+          <br /> built and inspire others <br />
+          to build similar fun <br />
+          projects.
+        </p>
+      </div>
+    </section>
+    <p class={styles.bodyText} id={styles.centerText}>
+      Beginners are highly encouraged to attend these workshops to learn
+      programming and new skills.
+      <br />
+      Check out the current uploaded{" "}
+      <span
+      class={styles.link}
+      onClick={() => window.open("workshops", "_self")}>
+      WORKSHOPS
+    </span>{" "}
+    .
+  </p>
+</section>
+<div id={styles.greenDivider}></div>
+<section class={styles.section}>
+  <h1 class={styles.sectionSubtitle} id={styles.beyondSubtitle}>
+    Beyond Meetings
+  </h1>
+  <p class={styles.bodyText}>
+    Our club goes beyond simple meetings and we host a ton of fun
+    events. Some events we are planning for this year include:
+    <ul>
+      <li>2-day Hackathons</li>
+      <li>Hack Nights</li>
+      <li>Coding Challenges</li>
+      <li>Hour of Code</li>
+      <li>TED Talks</li>
+      <li>programming Competitions</li>
+    </ul>
+    For more information on upcoming events, check out the<span class={styles.link} onClick={() => window.open("events", "_self")}>
+      EVENTS
+    </span>
+    page.
+  </p>
+</section>
+<section class={styles.section}>
+  <h1 class={styles.sectionTitle}>Requirements</h1>
+  <p class={styles.bodyText}>
+    In order to be eligible to join Reagan CS Club, you must be a
+    student of Ronald Reagan High School in San Antonio, Texas and be
+    in good standing with NEISD policies and government laws. Those
+    are the only requirements for joining as a new member.
+    <br />
+    <br /> In order to maintain or renew membership, there is one
+    additional requirement other than the ones mentioned above. You
+    must attend 5 events or workshops held or approved by Reagan Hack
+    Club every school semester. More information is in the club{" "}
+    <span
+    class={styles.link}
+    onClick={() => window.open(constitution_link, "_blank")}>
+    CONSTITUTION
+  </span>{" "}
+  . If you meet the requirements,{" "}
+  <span
+  class={styles.link}
+  onClick={() => window.open(apply_link, "_blank")}>
+  APPLY
+</span>{" "}
+now to be a part of the club! This year, there are no fees for any
+members.
+</p>
+</section>
+</main> -->
+</html>
