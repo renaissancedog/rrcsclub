023086fc0950fab6deee302a7930c0f8c8b77355
William Liu <orangetnt827@gmail.com>
2024-10-11 22:28:39 -0500

    alpha

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<script lang="ts">
+  import { base } from '$app/paths';
+</script>
 <h1 class="text-center">Welcome to SvelteKit</h1>
 <p class="bg-blue-600 p-2">Visit <a class="underline text-blue-100" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<a class="underline text-blue-500 pr-4" href="{base}/main">main</a>
