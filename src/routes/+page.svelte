1712b1ae13bac71689454049700e6915470b4508
William Liu <orangetnt827@gmail.com>
2024-10-11 21:34:09 -0500

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1 class="text-center">Welcome to SvelteKit</h1>
+<p class="bg-blue-600 p-4">Visit <a class="underline text-blue-100" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
