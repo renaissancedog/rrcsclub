8903d5e665ce5be7c6ce3555675173e734203e02
William Liu <orangetnt827@gmail.com>
2024-10-12 10:38:46 -0500

    update readme, add favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+  // import { base } from '$app/paths';
   // import Navbar from "../components/navbar";
   const apply_link = "https://forms.gle/i3ncV7JumfaodDXK8";
   const contact_link = "https://forms.gle/QkcoFtm9A6woxrs28";
@@ -9,7 +10,6 @@
 <!-- <script defer data-domain="rrcsclub.vercel.app" src="https://plausible.io/js/plausible.js"></script> -->
 <title>Ronald Reagan High School Computer Science Club</title>
 <meta name='description' content='The home page for the Ronald Reagan High School Computer Science club. We are a club for creative coding and Computer Science exploration. Join us to gain pratical experience in Computer Science and serve the community while having tons of fun!'/>
-<!-- <link rel='icon' href='/favicon.ico' /> -->
 <link rel='preconnect' href='https://fonts.gstatic.com' />
 <div class="bg-white flex flex-col justify-center items-center w-screen">
   <!-- <Navbar /> -->
