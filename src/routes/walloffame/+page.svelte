18029447fc5b5b113152e65ac051479bb4e87e87
William Liu <orangetnt827@gmail.com>
2024-10-14 13:23:56 -0500

    add margin on sides of cards

--- src/routes/walloffame/+page.svelte
+++ src/routes/walloffame/+page.svelte
@@ -9,7 +9,9 @@
 <div class="bg-teal-900 flex flex-col justify-center items-center w-screen">
   <Navbar {base}/>
   <h1 class='my-4 text-center text-7xl text-white'>Wall of Fame</h1>
-  <Card authorName="William Liu" projName="Reagan CS Club Website" desc="The website of the Reagan High School Computer Science Club!" link="https://renaissancedog.github.io/rrcsclub/"/>
-  <Card authorName="Sarthak Dayal" projName="Former Reagan CS Club Website" desc="The former website of the Club." link="https://rr.hackclub.com/"/>
+  <div class="m-2">
+    <Card authorName="William Liu" projName="Reagan CS Club Website" desc="The website of the Reagan High School Computer Science Club!" link="https://renaissancedog.github.io/rrcsclub/"/>
+    <Card authorName="Sarthak Dayal" projName="Former Reagan CS Club Website" desc="The former website of the Club." link="https://rr.hackclub.com/"/>
+  </div>
 </div> 
 <div class="h-[90vh] bg-teal-900"></div>
