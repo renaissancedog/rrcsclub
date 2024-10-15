4c54c4deb5e8cb4cfce498db19f27b940b9d337f
William Liu <orangetnt827@gmail.com>
2024-10-14 20:59:41 -0500

    css

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,10 +1,13 @@
 <script>
   export let authorName, projName, desc, link;
 </script>
-<section class="flex flex-wrap flex-col lg:flex-row items-center justify-around w-full">
-  <div class="bg-white font-sans my-2 text-center rounded-xl">
-    <h2 class='px-2 mt-4 text-3xl text-white bg-teal-700'>{projName}</h2>
-    <h2 class='px-2 mb-2 text-2xl text-white bg-teal-700'>Created by {authorName}</h2>
-    <p class='m-4 text-3xl'>{desc} <br> <a class="underline" target="_blank" rel="noopener noreferrer"  href={link}>{link}</a></p>
+
+<section class="flex flex-col lg:flex-row items-center justify-around w-full p-4">
+  <div class="bg-white font-sans my-4 text-center rounded-xl shadow-md w-full lg:w-1/2">
+    <h2 class='px-4 py-2 text-2xl lg:text-3xl text-white bg-teal-700 rounded-t-xl'>{projName}</h2>
+    <h2 class='px-4 py-2 text-xl lg:text-2xl text-white bg-teal-700'>{authorName}</h2>
+    <p class='m-4 text-lg lg:text-2xl'>{desc} <br> 
+      <a class="underline text-blue-600" target="_blank" rel="noopener noreferrer" href={link}>{link}</a>
+    </p>
   </div>
 </section>
--- src/routes/walloffame/+page.svelte
+++ src/routes/walloffame/+page.svelte
@@ -7,12 +7,24 @@
 <title>Reagan HS Computer Science Club</title>
 <meta name='description' content='The home page for the Ronald Reagan High School Computer Science club. We are a club for creative coding and Computer Science exploration. Join us to gain practical experience in Computer Science while having tons of fun!'/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<div class="bg-teal-900 flex flex-col justify-center items-center w-screen">
-  <Navbar {base}/>
-  <h1 class='my-4 text-center text-7xl text-white'>Wall of Fame</h1>
-  <div class="flex flex-wrap">
-    <Card authorName="William Liu" projName="Reagan CS Club Website" desc="The website of the Reagan High School Computer Science Club!" link="https://renaissancedog.github.io/rrcsclub/"/>
-    <Card authorName="Sarthak Dayal" projName="Former Reagan CS Club Website" desc="The former website of the Club." link="https://rr.hackclub.com/"/>
+
+<div class="bg-teal-900 flex flex-col justify-center items-center w-full">
+  <Navbar {base} />
+  <h1 class='my-4 text-center text-4xl sm:text-5xl lg:text-7xl text-white'>Wall of Fame</h1>
+  <div class="flex flex-wrap justify-center w-full px-4">
+    <Card 
+      authorName="William Liu" 
+      projName="Reagan CS Club Website" 
+      desc="The website of the Reagan High School Computer Science Club!" 
+      link="https://renaissancedog.github.io/rrcsclub/" 
+    />
+    <Card 
+      authorName="Sarthak Dayal" 
+      projName="Former Reagan CS Club Website" 
+      desc="The former website of the Club." 
+      link="https://rr.hackclub.com/" 
+    />
   </div>
-</div> 
-<div class="h-[90vh] bg-teal-900"></div>
+</div>
+
+<div class="h-[50vh] lg:h-[90vh] bg-teal-900"></div>
