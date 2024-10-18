63903005ab4bdebeb76949f9655e77486f68e920
William Liu <orangetnt827@gmail.com>
2024-10-17 21:19:05 -0500

    fix navbar working weird between sm and lg breakpoints

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="font-mono text-2xl h-auto p-4 bg-gray-900 text-center w-full flex flex-col sm:flex-row items-center justify-center text-white">
-  <ul class="lg:space-x-8 flex flex-col sm:flex-row items-center">
+  <ul class="sm:space-x-8 flex flex-col sm:flex-row items-center">
     <li>
       <a class="cursor-pointer text-white uppercase" href="{base}">HOME</a>
     </li>
